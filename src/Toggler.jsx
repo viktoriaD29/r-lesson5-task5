6ca517107b69ea9ce9d6a6e00ef01a50f2f7aab7
Viktoria <doduchenko29@gmail.com>
2021-07-11 11:57:37 +0300

    add

--- src/Toggler.jsx
+++ src/Toggler.jsx
@@ -2,21 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Toggler extends React.Component {
-  handelClick(e) {
-    if (e.target.textContent === 'Off') {
-      e.target.textContent = 'On';
-    } else {
-      e.target.textContent = 'Off';
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      button: false,
+    };
   }
+
+  handelClick = () => {
+    this.setState({
+      button: !this.state.button,
+    });
+  }
+  // onButton() {
+  //   if (this.state.button === 'Off') {
+  //     this.state.button = 'On';
+  //   } else {
+  //     this.state.button = 'Off';
+  //   }
+  // }
 
   render() {
     return (
-      <button className="toggler" onClick={this.handelClick}>
-        Off
+      <button className="toggler" onClick={() => this.handelClick}>
+        {this.state.button ? 'On' : 'Off'}
       </button>
     );
   }
 }
 
 export default Toggler;
+
+// handelClick(e) {
+//   if (e.target.textContent === 'Off') {
+//     e.target.textContent = 'On';
+//   } else {
+//     e.target.textContent = 'Off';
+//   }
+// }
